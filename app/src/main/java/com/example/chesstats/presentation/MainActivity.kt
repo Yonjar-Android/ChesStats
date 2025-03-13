0e48452f42f9a560d33895c14daf31758a2c6018
Yonjar <juan132y@gmail.com>
2025-03-13 09:35:41 -0600

    First configurations

---
+++ app/src/main/java/com/example/chesstats/presentation/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.chesstats.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.chesstats.ui.theme.ChesStatsTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ChesStatsTheme {
+
+            }
+        }
+    }
+}
